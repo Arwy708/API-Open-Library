336d782f12b313348ccc933d5bb138aaedd62cdf
Arwy07 <erwann.sueur@ynov.com>
2026-09-21 12:03:51 +0200

    .

--- API-Library/src/main.tsx
+++ API-Library/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
+import { FournisseurFavoris } from "./contexte/ContexteFavoris";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <FournisseurFavoris>
         <App />
+      </FournisseurFavoris>
     </BrowserRouter>
   </React.StrictMode>
 );
